6db3541a7f4b633045d2a76153f9b695edefe26c
KivenWick <221060209+sly3339146602@users.noreply.github.com>
2026-03-12 17:18:35 +0800

    feat: update transfer and withdraw logic, update home game categories and strings

--- lib/gen/strings.g.dart
+++ lib/gen/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 3
 /// Strings: 108 (36 per locale)
 ///
-/// Built on 2026-03-10 at 11:56 UTC
+/// Built on 2026-03-12 at 08:55 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
